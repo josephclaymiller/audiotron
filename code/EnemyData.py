14970a992f8812a5b30aa57cefa7d9b75a7fab50
nottingham.dan <nottingham.dan@df45ab90-1348-0410-a7a9-55bda8f8a9ff>
2008-04-09 20:18:50 +0000

    More enemy data

--- code/EnemyData.py
+++ code/EnemyData.py
@@ -1,8 +1,14 @@
+measures = 17
+sixteenths = measures * 16
+
 enemyData = \
 {
 	"testEnemy":
 	{
-		"model":	"emenytb_t1",
-		"music":	"some.wav",
+		"model":		"emenytb_t1",
+		"music":		"some.wav",
+		"lighting":		('ALred', 'DLRblue', 'DLBwhite'),
+		"beatsPulse":	[x * 4 for x in range(sixteenths / 4)],
+		"beatsLight":	[],
 	},
 }
