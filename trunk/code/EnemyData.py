28a725f25d0ecfab6fbf435a0922c7cf3af76683
nottingham.dan <nottingham.dan@df45ab90-1348-0410-a7a9-55bda8f8a9ff>
2008-04-09 21:38:49 +0000

    scale enemies in data

--- trunk/code/EnemyData.py
+++ trunk/code/EnemyData.py
@@ -3,12 +3,13 @@ sixteenths = measures * 16
 
 enemyData = \
 {
-	"testEnemy":
+	'testEnemy':
 	{
-		"model":		"enemy_tb_trans_am_incan",
-		"music":		"some.wav",
-		"lighting":		('ALred', 'DLRblue', 'DLBwhite'),
-		"beatsPulse":	[x * 4 for x in range(sixteenths / 4)],
-		"beatsLight":	[],
+		'model':		'enemy_tb_trans_am_incan',
+		'scale':		0.1,
+		'music':		'some.wav',
+		'lighting':		('ALred', 'DLRblue', 'DLBwhite'),
+		'beatsPulse':	[x * 4 for x in range(sixteenths / 4)],
+		'beatsLight':	[],
 	},
 }
